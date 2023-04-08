768bb0bb65ec8a3d15048999efc0b4b2e34b44eb
Shivam-Omen <srvcode@gmail.com>
2023-04-08 20:55:43 +0530

    Adding XML Spring implementation of Spring Batch for converting csv file to xml

---
+++ Spring-Batch-Intro/src/test/java/com/srvcode/springbatch/SpringBatchIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srvcode.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
